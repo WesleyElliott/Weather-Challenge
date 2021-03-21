8c8250089c27177c48e8f7f65b690bd4cfe98cac
Wesley <wesleyjasonelliott@gmail.com>
2021-03-21 20:12:28 +0200

    Add transparent status bar with inset padding

--- app/src/main/java/com/wesleyelliott/weather/MainActivity.kt
+++ app/src/main/java/com/wesleyelliott/weather/MainActivity.kt
@@ -15,22 +15,40 @@
  */
 package com.wesleyelliott.weather
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
+import com.wesleyelliott.weather.ui.choose.ChooseScreen
 import com.wesleyelliott.weather.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeStatusBarTransparent()
         setContent {
-            MyTheme {
-                MyApp()
+            ProvideWindowInsets {
+                MyTheme(darkTheme = false) {
+                    MyApp()
+                }
             }
         }
     }
@@ -39,8 +57,41 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val clicked = remember {
+        mutableStateOf(false)
+    }
+
+    val offset = animateOffsetAsState(
+        targetValue = if (clicked.value) Offset(0f, -1000f) else Offset(0f, 0f),
+        animationSpec = tween(
+            durationMillis = 500,
+            delayMillis = 250,
+            easing = LinearOutSlowInEasing
+        )
+    )
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        if (offset.value.y == -1000f) {
+        } else {
+            ChooseScreen(
+                modifier = Modifier.statusBarsPadding().offset(
+                    y = offset.value.y.dp
+                )
+            ) {
+                clicked.value = true
+            }
+        }
+
+    }
+}
+
+fun Activity.makeStatusBarTransparent() {
+    window.apply {
+        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        statusBarColor = Color.TRANSPARENT
     }
 }
 
