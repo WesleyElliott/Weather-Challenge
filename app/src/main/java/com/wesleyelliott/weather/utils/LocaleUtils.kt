3c5fc14de2169c3a44a04f303d5242bc46c0d0ff
Wesley <wesleyjasonelliott@gmail.com>
2021-03-22 20:34:20 +0200

    More comments

--- app/src/main/java/com/wesleyelliott/weather/utils/LocaleUtils.kt
+++ app/src/main/java/com/wesleyelliott/weather/utils/LocaleUtils.kt
@@ -7,16 +7,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.staticCompositionLocalOf
 import java.util.*
 
+/**
+ * This composition local allows child composables of the app to access the current measurement
+ * unit used by the app. It defaults to the system settings, but can be changed during an app
+ * session to the users preference.
+ * This is reset on every app launch.
+ */
 val LocalUnitProvider = staticCompositionLocalOf {
     mutableStateOf(Locale.getDefault().getLocaleUnits())
 }
 
+/**
+ * Our own representation of the different measurement units available
+ */
 enum class MeasurementUnit {
     METRIC,
     IMPERIAL,
     UK // They just have to be different...
 }
 
+/**
+ * Helper function to get the unit system the phone uses, based on the user's selected [Locale]
+ */
 fun Locale.getLocaleUnits(): MeasurementUnit {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
         return when (LocaleData.getMeasurementSystem(ULocale.forLocale(this))) {
